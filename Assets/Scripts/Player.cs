3030e3014eab3dbde1b2c4fc492167db4af7dae7
GwenRspl <gwenaelle.rispal@gmail.com>
2020-08-30 15:16:33 +0200

    Add animation state to running

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,12 +4,19 @@ using UnityEngine;
 
 public class Player : MonoBehaviour {
 
+    //Config
     [SerializeField] float runSpeed = 5f;
 
+    //State
+    bool isAlive = true;
+
+    //Cache
     private Rigidbody2D rigidBody;
+    private Animator animator;
 
     private void Start() {
         this.rigidBody = GetComponent<Rigidbody2D>();
+        this.animator = GetComponent<Animator>();
     }
 
     private void Update() {
@@ -21,6 +28,10 @@ public class Player : MonoBehaviour {
         float controlThrow = Input.GetAxis("Horizontal");
         Vector2 playerVelocity = new Vector2(controlThrow * this.runSpeed, this.rigidBody.velocity.y);
         this.rigidBody.velocity = playerVelocity;
+
+        bool playerHasHorizontalSpeed = Mathf.Abs(this.rigidBody.velocity.x) > Mathf.Epsilon;
+        this.animator.SetBool("running", playerHasHorizontalSpeed);
+
     }
 
     private void FlipSprite() {
