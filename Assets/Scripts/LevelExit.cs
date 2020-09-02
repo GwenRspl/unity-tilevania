42894f4d4e155fde7d1e0a16f9e8a85c1dd52f85
GwenRspl <gwenaelle.rispal@gmail.com>
2020-09-02 22:46:41 +0200

    Add start and end to game

---
+++ Assets/Scripts/LevelExit.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelExit : MonoBehaviour {
+    [SerializeField] float levelExitSlowMoFactor = 0.2f;
+    [SerializeField] float delay = 2f;
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        StartCoroutine(LoadNextLevel());
+    }
+
+    IEnumerator LoadNextLevel() {
+        Time.timeScale = this.levelExitSlowMoFactor;
+        yield return new WaitForSecondsRealtime(this.delay);
+        Time.timeScale = 1f;
+        var currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex + 1);
+    }
+}
