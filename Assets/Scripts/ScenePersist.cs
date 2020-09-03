69ec032971a192ef9d7f376eb9440a09230ed4e4
GwenRspl <gwenaelle.rispal@gmail.com>
2020-09-03 22:01:42 +0200

    Pickups scene persistence

---
+++ Assets/Scripts/ScenePersist.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ScenePersist : MonoBehaviour {
+    private int startingSceneIndex;
+
+    private void Awake() {
+        int numberOfScenePersist = FindObjectsOfType<ScenePersist>().Length;
+        if (numberOfScenePersist > 1) {
+            Destroy(this.gameObject);
+        } else {
+            DontDestroyOnLoad(this.gameObject);
+        }
+    }
+
+    private void Start() {
+        this.startingSceneIndex = SceneManager.GetActiveScene().buildIndex;
+    }
+
+    private void Update() {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        if (currentSceneIndex != this.startingSceneIndex) {
+            Destroy(this.gameObject);
+        }
+    }
+}
